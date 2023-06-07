3334d0fc9144a534264685d6016f81ebd111991a
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2023-06-07 12:34:38 +0200

    feat: reports in 2col + swap button edit-validate

--- src/features/report/Report.js
+++ src/features/report/Report.js
@@ -18,16 +18,21 @@ const Report = ({ report }) => {
   const [nextVisitItems, setNextVisitItems] = useState(report.nextVisitItems);
   const [nextVisitRevenue, setNextVisitRevenue] = useState(report.nextVisitRevenue);
 
-  const handleEdit = () => {
-    setIsEditing(!isEditing)
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setIsEditing(true)
   };
 
-  const handleDelete = () => {
-    deleteReport(report);
+  const handleDelete = async () => {
+    const deleteResp = await deleteReport(report);
+    if (deleteResp) {
+      window.location.reload();
+    }
   };
 
   const handleSave = async (e) => {
     e.preventDefault();
+    setIsEditing(false)
     await updateReport(report, {
       customerAddress,
       customerName,
@@ -47,143 +52,150 @@ const Report = ({ report }) => {
       <div className="card mb-3">
         <div className="card-body">
           <h5 className="card-title">Rapport</h5>
+          <div className="row">
+            <div className="col"><div className="form-group">
+              <label>Nom :</label>
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={customerName}
+                  readOnly={!isEditing}
+                  onChange={(e) => setCustomerName(e.target.value)}
+                />
+              </div>
+            </div>
 
-          <div className="form-group">
-            <label>Nom :</label>
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                value={customerName}
-                readOnly={!isEditing}
-                onChange={(e) => setCustomerName(e.target.value)}
-              />
+              <div className="form-group">
+                <label>Adresse :</label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={customerAddress}
+                    readOnly={!isEditing}
+                    onChange={(e) => setCustomerAddress(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label>Contact :</label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={customerContact}
+                    readOnly={!isEditing}
+                    onChange={(e) => setCustomerContact(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label>VisitDate :</label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={visitDate}
+                    readOnly={!isEditing}
+                    onChange={(e) => setVisitDate(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label>ReportText :</label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={reportBody}
+                    readOnly={!isEditing}
+                    onChange={(e) => setReportBody(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col">
+
+              <div className="form-group">
+                <label>NumArticles :</label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={orderedItems}
+                    readOnly={!isEditing}
+                    onChange={(e) => setOrderedItems(parseInt(e.target.value))}
+                  />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label>Revenue :</label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={revenue}
+                    readOnly={!isEditing}
+                    onChange={(e) => setRevenue(parseFloat(e.target.value))}
+                  />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label>NextVisitDate :</label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={nextVisitDate}
+                    readOnly={!isEditing}
+                    onChange={(e) => setNextVisitDate(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label>ForecastNumArticles :</label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={nextVisitItems}
+                    readOnly={!isEditing}
+                    onChange={(e) => setNextVisitItems(parseInt(e.target.value))}
+                  />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label>ForecastRevenue :</label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={nextVisitRevenue}
+                    readOnly={!isEditing}
+                    onChange={(e) => setNextVisitRevenue(parseFloat(e.target.value))}
+                  />
+                </div>
+              </div>
+
             </div>
           </div>
-
-          <div className="form-group">
-            <label>Adresse :</label>
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                value={customerAddress}
-                readOnly={!isEditing}
-                onChange={(e) => setCustomerAddress(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <div className="form-group">
-            <label>Contact :</label>
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                value={customerContact}
-                readOnly={!isEditing}
-                onChange={(e) => setCustomerContact(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <div className="form-group">
-            <label>VisitDate :</label>
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                value={visitDate}
-                readOnly={!isEditing}
-                onChange={(e) => setVisitDate(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <div className="form-group">
-            <label>ReportText :</label>
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                value={reportBody}
-                readOnly={!isEditing}
-                onChange={(e) => setReportBody(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <div className="form-group">
-            <label>NumArticles :</label>
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                value={orderedItems}
-                readOnly={!isEditing}
-                onChange={(e) => setOrderedItems(parseInt(e.target.value))}
-              />
-            </div>
-          </div>
-
-          <div className="form-group">
-            <label>Revenue :</label>
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                value={revenue}
-                readOnly={!isEditing}
-                onChange={(e) => setRevenue(parseFloat(e.target.value))}
-              />
-            </div>
-          </div>
-
-          <div className="form-group">
-            <label>NextVisitDate :</label>
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                value={nextVisitDate}
-                readOnly={!isEditing}
-                onChange={(e) => setNextVisitDate(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <div className="form-group">
-            <label>ForecastNumArticles :</label>
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                value={nextVisitItems}
-                readOnly={!isEditing}
-                onChange={(e) => setNextVisitItems(parseInt(e.target.value))}
-              />
-            </div>
-          </div>
-
-          <div className="form-group">
-            <label>ForecastRevenue :</label>
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                value={nextVisitRevenue}
-                readOnly={!isEditing}
-                onChange={(e) => setNextVisitRevenue(parseFloat(e.target.value))}
-              />
-            </div>
-          </div>
-
-          <button type="button" className="btn btn-primary" onClick={() => handleEdit()}>
-            Modifier
-          </button>
-          <button type="submit" className="btn btn-success">
-            Valider
-          </button>
+          {
+            isEditing
+              ? <button type="submit" className="btn btn-success">
+                Valider
+              </button>
+              : <button type="button" className="btn btn-primary" onClick={(e) => handleEdit(e)}>
+                Modifier
+              </button>
+          }
           <button type="button" className="btn btn-danger" onClick={() => handleDelete()}>
             Supprimer
           </button>
